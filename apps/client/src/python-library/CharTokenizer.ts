73a969080b1aa61fb74479cea11cd69b321037ea
training4usaco <liualex54321@gmail.com>
2026-01-30 17:58:12 -0800

    fix: refactor tokenizer and fix some static values

--- apps/client/src/python-library/CharTokenizer.ts
+++ apps/client/src/python-library/CharTokenizer.ts
@@ -1,28 +1,37 @@
-export const CharTokenizer =
-    'class CharTokenizer:\n' +
-    '  def __init__(self, data):\n' +
-    '    # Find all unique characters\n' +
-    '    chars = sorted(list(set("".join(data))))\n' +
-    '    self.stoi = {s:i+1 for i,s in enumerate(chars)}\n' +
-    '    self.stoi["."] = 0\n' +
-    '    self.itos = {i:s for s,i in self.stoi.items()}\n' +
-    '    self.vocab_size = len(self.itos)\n' +
-    '    self.data = data\n' +
-    '\n' +
-    '  def encode(self, word):\n' +
-    '    return [self.stoi[c] for c in word + "."]\n' +
-    '\n' +
-    '  def decode(self, tokens):\n' +
-    '    return "".join([self.itos[i] for i in tokens])\n' +
-    '\n' +
-    '  def export_dataset(self, block_size):\n' +
-    '    xs, ys = [], []\n' +
-    '    for w in self.data:\n' +
-    '      encoded = self.encode(w)\n' +
-    '      # Create sliding window\n' +
-    '      context = [0] * block_size\n' +
-    '      for idx in encoded:\n' +
-    '        xs.append(context)\n' +
-    '        ys.append(idx)\n' +
-    '        context = context[1:] + [idx]\n' +
-    '    return torch.tensor(xs), torch.tensor(ys)\n'
+export const CharTokenizer = `
+class CharTokenizer:
+  def __init__(self, data):
+    self.data = data
+  
+    # Find all unique characters
+    data_chars = sorted(list(set("".join(data))))
+
+    self.eos_token = '<STOP>'
+
+    self.vocab_list = [self.eos_token] + data_chars
+
+    self.stoi = {s: i for i, s in enumerate(self.vocab_list)}
+    self.itos = {i: s for s, i in self.stoi.items()}
+
+    self.vocab_size = len(self.vocab_list)
+
+  def encode(self, word):
+    return [self.stoi[c] for c in word] + [self.stoi[self.eos_token]]
+
+  def decode(self, tokens):
+    decoded_list = []
+    for i in tokens:
+        decoded_list.append(self.itos[i])
+    return "".join(decoded_list)
+
+  def export_dataset(self, block_size):
+    xs, ys = [], []
+    for w in self.data:
+      encoded = self.encode(w)
+      # Create sliding window
+      context = [self.stoi[self.eos_token]] * block_size
+      for idx in encoded:
+        xs.append(context)
+        ys.append(idx)
+        context = context[1:] + [idx]
+    return torch.tensor(xs), torch.tensor(ys)`;
--- apps/client/src/python-library/GenerateInference.ts
+++ apps/client/src/python-library/GenerateInference.ts
@@ -1,19 +1,22 @@
 export const GenerateInference = (numSamples: string, modelVar: string) => {
-    return `for _ in range(${numSamples}):
+    return `print(f"Generating ${numSamples} samples...")
+stop_token_idx = ${modelVar}.tokenizer.stoi[${modelVar}.tokenizer.eos_token]
+for _ in range(${numSamples}):
     out = []
-    context = [0] * ${modelVar}.context_len
+    context = [stop_token_idx] * ${modelVar}.context_len
     while True:
           logits = ${modelVar}(torch.tensor([context]))
           probs = F.softmax(logits, dim=1)
           ix = torch.multinomial(probs, num_samples=1).item()
+          
+          if ix == stop_token_idx:
+                break
 
           context = context[1:] + [ix]
           out.append(ix)
-
-          if ix == 0:
-                break
-
+          
     print(''.join(${modelVar}.tokenizer.itos[i] for i in out))
+print()
 
 ` 
 }
